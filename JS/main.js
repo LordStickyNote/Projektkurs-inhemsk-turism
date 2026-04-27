54e677f930313b1744a180443be8cde8986903b3
Oskar Berntson <oskar.berntson@gmail.com>
2026-04-28 00:02:35 +0200

    Add code for fetching activity/attraction objects with button. Add renderSeeAndDo.js for later rendering in HTML.

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,22 @@
 import { getData } from "./api.js";
 import { categories } from "./categories.js";
 
-const attraction = categories.seeAndDo.sections[0]
-const see = await getData(attraction.controller);
+// const attraction = categories.seeAndDo.sections[0]
+// const see = await getData(attraction.controller);
 
-for (const name of see) {
-    console.log(name.name)
+// for (const name of see) {
+//     console.log(name.name)
+// }
+
+document.querySelector("button").addEventListener("click", loadSeeAndDo)
+
+async function loadSeeAndDo() {
+    for (const section of categories.seeAndDo.sections) {
+        const data = await getData(section.controller);
+
+        for (const name of data) {
+            console.log(name.name)
+        }
+    }
+    
 }
