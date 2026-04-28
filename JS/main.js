7ea58bea543e3a771beceabe8815f3bb8232202f
Oskar Berntson <oskar.berntson@gmail.com>
2026-04-28 21:23:42 +0200

    Add helpful comments in main.js

--- JS/main.js
+++ JS/main.js
@@ -10,8 +10,6 @@ document.querySelector("#foodBtn").addEventListener("click", loadFood)
 document.querySelector("#accommodationBtn").addEventListener("click", loadAccommodation)
 
 const seeFilter = document.getElementById("seeFilter");
-seeFilter.addEventListener("change", loadSeeAndDo)
-
 const container = document.getElementById("results");
 
 const activityFilters = document.getElementById("activityFilters");
@@ -21,8 +19,10 @@ const childFriendly = document.getElementById("childFriendly");
 const involvesAnimals = document.getElementById("involvesAnimals");
 const involvesWater = document.getElementById("involvesWater");
 
+// Sparar ALLA activites från API (innan filtrering)
 let allActivities = [];
 
+seeFilter.addEventListener("change", loadSeeAndDo)
 const inputs = activityFilters.querySelectorAll("select, input");
 for (const input of inputs) {
     input.addEventListener("change", applyActivityFilters)
@@ -31,23 +31,30 @@ for (const input of inputs) {
 async function loadSeeAndDo() {
     const selectedType = seeFilter.value;
 
+    // Visar activity-filter bara om "activity" är valt
     activityFilters.hidden = selectedType !== "activity";
     seeFilter.hidden = false;
 
+    // Array som innehåller sektioner med titel + data från SMAPI
     const sectionsData = [];
 
+    // Loopar igenom sections (activity + attraction, se categories.js)
     for (const section of categories.seeAndDo.sections) {
 
+        // Hoppar över fel kategori (t.ex. om man endast valt "Aktiviteter")
         if (selectedType !== "all" && section.controller !== selectedType) {
             continue;
         }
 
+        // Hämtar data från SMAPI. Items är en array från SMAPI med de olika platserna
         const items = await getData(section.controller, section.filters)
 
+        // Sparar activities separat så vi kan filtrera dom senare
         if (section.controller === "activity") {
             allActivities = items;
         }
 
+        // Struktur för render-funktionen. Innehåller titeln för sektionen + alla objekt från SMAPI
         sectionsData.push({
             title: section.title,
             items: items
@@ -57,6 +64,7 @@ async function loadSeeAndDo() {
     renderSeeAndDo(sectionsData, container)
 }
 
+// Körs när användaren ändrar activity-filter
 function applyActivityFilters() {
     const filters = {
         type: activityType.value,
@@ -66,8 +74,10 @@ function applyActivityFilters() {
         involvesWater: involvesWater.checked
     };
 
+    // Filtrerar redan hämtad data
     const filtered = filterActivities(allActivities, filters);
 
+    // Renderar den filtrerade activity sektionen
     renderSeeAndDo([
         {
             title: "Aktiviteter",
