786777d7c5e565b1c2ce741d847db0795af32fb4
Oskar Berntson <oskar.berntson@gmail.com>
2026-05-07 21:21:51 +0200

    Add current_page and per_page to fetch arguments in api.js

--- JS/api.js
+++ JS/api.js
@@ -2,13 +2,18 @@ import { API_KEY } from "./config.js";
 
 const BASE_URL = "https://smapi.lnu.se/api/"
 
-export async function getData(controller, filters = {}) {
+export async function getData(controller, filters = {}, page = null, perPage = null) {
     const params = new URLSearchParams({
         api_key: API_KEY,
         controller: controller,
         method: "getall",
         ...filters
     });
+
+    if (page && perPage) {
+        params.set("current_page", page);
+        params.set("per_page", perPage);
+    }
 
     const url = `${BASE_URL}?${params}`;
 
