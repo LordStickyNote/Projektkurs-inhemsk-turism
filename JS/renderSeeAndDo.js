81ce18e6e0dbb52ff6071e60450f1d00e1ed185b
LordStickyNote <asmastorp@gmail.com>
2026-05-19 20:01:56 +0200

    added lazy load on images

--- JS/renderSeeAndDo.js
+++ JS/renderSeeAndDo.js
@@ -15,7 +15,7 @@ export async function renderSeeAndDo(sections, container) {
 
       article.innerHTML = `
             <div class="card card-listing">
-        <img src="${imageUrl}" alt="Image of ${item.description}" />
+        <img src="${imageUrl}" loading="lazy" alt="Image of ${item.description}" />
         <span class="card-listing-content width-full">
           <h3>${item.name}</h3>
           <h4 class="text-faded">${item.city}</h4>
