820cf4202ded378a500546dc8fc6bcbf749194b1
LordStickyNote <asmastorp@gmail.com>
2026-05-19 17:28:30 +0200

    card-styling

--- JS/renderSeeAndDo.js
+++ JS/renderSeeAndDo.js
@@ -14,8 +14,7 @@ export async function renderSeeAndDo(sections, container) {
       const imageUrl = (await getPixabayImage(item.description, item.id)) || "./img/High_Chaparral_Theme_Park.jpg";
 
       article.innerHTML = `
-            <div class="card card-listing">
-        <img src="${imageUrl}" alt="Image of ${item.description}" />
+            <div class="card card-listing" style="background-image: url('${imageUrl}');">
         <span class="card-listing-content width-full">
           <h3>${item.name}</h3>
           <h4 class="text-faded">${item.city}</h4>
