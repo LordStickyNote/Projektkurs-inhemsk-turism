90dddf03d3245836a370a0de2034f247a13ef502
LordStickyNote <asmastorp@gmail.com>
2026-05-21 17:37:30 +0200

    filter redesign

--- JS/temp.js
+++ JS/temp.js
@@ -3,9 +3,11 @@ const openFilterBtn = document.getElementById("btn-float-SeeAndDo");
 const closeFilterBtn = document.getElementById("filterBtn");
 
 openFilterBtn.addEventListener("click", () => {
-    filterMenu.style.display = "flex";
-})
+  filterMenu.style.display = "flex";
+  openFilterBtn.style.display = "none";
+});
 
 closeFilterBtn.addEventListener("click", () => {
-    filterMenu.style.display = "none";
-})
+  filterMenu.style.display = "none";
+  openFilterBtn.style.display = "flex";
+});
