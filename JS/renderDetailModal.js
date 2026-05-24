6dd1d26e29a77fd38398929a4e5414d2f424e487
Oskar Berntson <oskar.berntson@gmail.com>
2026-05-24 14:50:34 +0200

    Add simple inneHTML rendering for testing

--- JS/renderDetailModal.js
+++ JS/renderDetailModal.js
@@ -9,4 +9,50 @@ export async function renderDetailModal(item) {
     modal.hidden = false;
 
     const imageUrl = (await getPixabayImage(item.description, item.id));
+
+    content.innerHTML = `
+    <section class="stack gap-6">
+
+      <div
+        class="card detail-hero"
+        style="background-image: url('${imageUrl}')"
+      >
+        <div class="card-listing-content">
+          <h1>${item.name}</h1>
+          <h4>${item.city}</h4>
+        </div>
+      </div>
+
+      <div class="card stack gap-4">
+
+        <div class="row row-between">
+          <h3>${item.description}</h3>
+
+          <span class="row">
+            <h4>${Math.trunc(item.rating || 0)}</h4>
+          </span>
+        </div>
+
+        <hr />
+
+        <div class="details">
+
+          <span>
+            <span class="text-faded">Typ</span>
+            <span>${item.description || "-"}</span>
+          </span>
+
+          <hr />
+
+          <span>
+            <span class="text-faded">Kommun</span>
+            <span>${item.city || "-"}</span>
+          </span>
+
+        </div>
+
+      </div>
+
+    </section>
+  `;
 }
