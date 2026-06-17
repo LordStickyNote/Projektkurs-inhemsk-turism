0ce97e2b4bc6b726615e7a3c8dbcfe6258c1b0cc
Saman-Glnzr <saman_gulnezer@hotmail.com>
2026-06-17 15:57:23 +0200

    FooterUpdate2

--- JS/footer.js
+++ JS/footer.js
@@ -7,16 +7,17 @@ footer.innerHTML = `
     <p>Vi hjälper dig planera!</p>
   </div>
 
-   <nav class="site-footer-links" aria-label="Sidfot navigation">
-    <a href="index.html">Home</a>
-        <a href="explore.html">Interaktiv map</a>
-    <a href="#top">Återvänd hem</a>
+  <nav class="site-footer-links" aria-label="Sidfot navigation">
+    <a href="index.html">Hemma</a>
+    <a href="explore.html">Interaktiv karta</a>
+    <a href="#top">Återvänd till toppen</a>
   </nav>
 
   <address class="site-footer-contact">
+    <h3>Kontaktinformation</h3>
     <a href="mailto:sg223vf@student.lnu.se">sg223vf@student.lnu.se</a>
     <a class="site-footer-phone" href="tel:0765677780">
-      <img src="img/telefon.svg " alt="" aria-hidden="true" />
+      <img src="img/telefon.svg" alt="" aria-hidden="true" />
       0765677780
     </a>
   </address>
